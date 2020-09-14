e22f5739c8cc189611598858ea861195c217285c
deeborges <deyvisonsilvaborges@gmail.com>
2020-09-14 16:27:27 -0300

    fix(utils): criando validadores de campos

---
+++ src/utils/validators/index.ts
@@ -0,0 +1,51 @@
+export abstract class Notifications {
+  notifications: Array<{ message: string }>;
+
+  constructor() {
+    this.notifications = Array<{ message: string }>();
+  }
+
+  addNotification(message: string): void {
+    this.notifications.push({ message: message });
+  }
+
+  IsTrue(value: any, message: string) {
+    if (value) {
+      this.addNotification(message);
+    }
+  }
+
+  IsRequired(value: any, message: string) {
+    if (!value || value.length <= 0) {
+      this.addNotification(message);
+    }
+  }
+
+  hasMinLen(value: string, min: number, message: string) {
+    if (!value || value.length < min) {
+      this.addNotification(message);
+    }
+  }
+
+  hasManLen(value, max, message: string) {
+    if (!value || value.length > max) {
+      this.addNotification(message);
+    }
+  }
+
+  hasFixedLen(value, len, message: string) {
+    if (value.length != len) {
+      this.addNotification(message);
+    }
+  }
+
+  isEmail(value, message: string) {}
+
+  get allNotifications(): Array<{ message: string }> {
+    return this.notifications;
+  }
+
+  valid(): boolean {
+    return this.notifications.length === 0;
+  }
+}
