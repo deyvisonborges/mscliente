6b29045c4a95384ba7ce0c12c6ea1fe536de13a5
deeborges <deyvisonsilvaborges@gmail.com>
2020-09-15 14:53:08 -0300

    refactor(entities): distribuicao de dados igual ao do mercadolivre

--- src/entities/DadosConta.ts
+++ src/entities/DadosConta.ts
@@ -4,7 +4,7 @@ import { EntityBase } from './EntityBase';
 @Entity({ name: 'dados_conta' })
 export class DadosConta extends EntityBase {
   @Column({ type: 'alphanum', length: 20 })
-  user?: string;
+  usuario?: string;
 
   @Column({ type: 'varchar', length: 40 })
   email?: string;
