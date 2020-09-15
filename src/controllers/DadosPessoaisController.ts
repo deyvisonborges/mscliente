af4b7e7ae1365574d510268d6c4008dedf541f69
deeborges <deyvisonsilvaborges@gmail.com>
2020-09-15 08:47:36 -0300

    refactor(controller dados pessoais): melhorias

--- src/controllers/DadosPessoaisController.ts
+++ src/controllers/DadosPessoaisController.ts
@@ -2,15 +2,18 @@ import { Request } from 'express';
 import { DadosPessoais } from '../entities/DadosPessoais';
 import { ControllerBase } from './ControllerBase';
 
-export default class DadosPessoaisController extends ControllerBase<DadosPessoais> {
-	constructor() {
-		super(DadosPessoais);
-	}
+class DadosPessoaisController extends ControllerBase<DadosPessoais> {
+  constructor() {
+    super(DadosPessoais);
+  }
 
-	async adicionarDadosPessoais(request: Request) {
-		let _data = <DadosPessoais>request.body;
-		super.IsRequired(_data.nome_cliente, 'Você precisa adicionar seu nome.');
-		super.IsRequired(_data.rg, 'O RG é obrigatório.');
-		super.IsRequired(_data.cpf, 'O CPF precisa ser informado.');
-	}
+  async adicionarDadosPessoais(request: Request) {
+    let _data = <DadosPessoais>request.body;
+    super.IsRequired(_data.nome_cliente, 'Você precisa adicionar seu nome.');
+    super.IsRequired(_data.rg, 'O RG é obrigatório.');
+    super.IsRequired(_data.cpf, 'O CPF precisa ser informado.');
+    return super.save(_data);
+  }
 }
+
+export default new DadosPessoaisController();
